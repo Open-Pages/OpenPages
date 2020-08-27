a09a0e9532f9c7a635f08e5c2cdd02afde4a0850
Absera <50250863+Absera@users.noreply.github.com>
2020-08-27 20:36:06 +0300

    Updated

--- js/script.js
+++ js/script.js
@@ -1,11 +1,4 @@
-window.onscroll = function() { scrollFunction() };
+$(window).scroll(function() {
 
-function scrollFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementById("navbar").style.padding = "1px";
-        document.getElementById("logo").style.fontSize = "25px";
-    } else {
-        document.getElementById("navbar").style.padding = "25px";
-        document.getElementById("logo").style.fontSize = "35px";
-    }
-}
+    $('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
+});
